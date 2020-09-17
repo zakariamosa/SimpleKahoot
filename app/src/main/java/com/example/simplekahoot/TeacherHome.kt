8e2207d9bff12eb7ea548122a0c6ba445f572967
zakariamosa <zakaria.mosa@iths.se>
2020-09-17 11:50:15 +0200

    some changes in Teacher home

--- app/src/main/java/com/example/simplekahoot/TeacherHome.kt
+++ app/src/main/java/com/example/simplekahoot/TeacherHome.kt
@@ -3,7 +3,10 @@ package com.example.simplekahoot
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.NestedScrollView
 
 class TeacherHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,22 @@ class TeacherHome : AppCompatActivity() {
             var intent= Intent(this,TeacherQuiz::class.java)
             startActivity(intent)
         }
+
+
+        var txtallquizes: EditText =findViewById(R.id.txtTeacherQuizes)
+        for (quz in allQuizes){
+            if (quz.QuizTeacher== currentTeacher){
+                txtallquizes.setText("${txtallquizes.text.toString()} \n Quiz Name: ${quz.quizName} ......Quiz Date: ${quz.quizDate.toString()}..... Quiz code: ${quz.quizCode}")
+            }
+        }
+
+
+        var btnlogout:Button=findViewById(R.id.btnLogout)
+        btnlogout.setOnClickListener(){
+            var intent=Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
