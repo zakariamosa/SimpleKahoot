390be3f74093c77bec95f6a801525c359e93c3cf
zakariamosa <zakaria.mosa@iths.se>
2020-09-18 22:59:35 +0200

    first student question some changes

---
+++ app/src/main/java/com/example/simplekahoot/TrueAnswer.kt
@@ -0,0 +1,11 @@
+package com.example.simplekahoot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrueAnswer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_true_answer)
+    }
+}
